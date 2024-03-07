0218077a858dda62cffa3ccac84ffebde8e6ba45
hunsang jo <johunsang@gmail.com>
2024-03-07 19:07:03 +0900

    88

---
+++ service/test.js
@@ -0,0 +1,41 @@
+const request = require("request");
+const uuidv4 = require("uuid/v4");
+const sign = require("jsonwebtoken").sign;
+const crypto = require("crypto");
+const { login } = require("./auth");
+const queryEncode = require("querystring").encode;
+
+exports.login = (id, password) => {
+  return new Promise((resolve, reject) => {
+    const body = {
+      vendorId: id,
+      password: password,
+    };
+
+    const options = {
+      method: "POST",
+      url: "https://admin.coudae.kr/chrome/login",
+      json: body,
+    };
+
+    request(options, (error, response, body) => {
+      if (error) {
+        console.log("error", error);
+        console.error(error);
+        resolve({
+          success: false,
+          message: error.message,
+        });
+      } else {
+        console.log(body);
+        resolve({
+          success: true,
+          message: "success",
+          data: body,
+        });
+      }
+    });
+  });
+};
+
+login("A00878694", "a1234567").then((result) => console.log(result));
